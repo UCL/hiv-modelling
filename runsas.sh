d0434750d20f6c5d63de895c1215fafe8c234923
cceapsc <cceapsc@login12.myriad.ucl.ac.uk>
2020-10-08 14:15:47 +0100

    HPC ready

---
+++ runsas.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -l
+#$ -l h_rt=08:00:00
+#$ -l mem=6G
+#$ -l tmpfs=15G
+#S -N hivtestrun
+#$ -wd /home/cceapsc/Scratch
+cd $TMPDIR
+module load sas/9.4/64
+cp $HOME/sas/hiv-modelling/model_runner.sas .
+cp $HOME/sas/hiv-modelling/test_hiv_model.sas .
+
+sas model_runner.sas
+
+
