1d0df9bab5b969636ee354f597f251fa54480765
Peter Schmidt <peter.schmidt@ucl.ac.uk>
2020-10-28 11:47:04 +0000

    Update script acc to discussion with Heather

--- current_model.sh
+++ current_model.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #$ -S /bin/bash
 #$ -l h_rt=08:00:00
-#$ -pe smp 4
+#$ -pe smp 16
 #$ -t 1-100
 #$ -A HIVSynthMod
 #$ -l mem=13G
@@ -12,7 +12,5 @@
 #$ -e /dev/null
 #$ -l tmpfs=100G
 module load sas/9.4/64
-cd $TMPDIR
-cp $HOME/sas/hiv-modelling/hiv_synthesis.sas .
 sleep $[ ( $RANDOM % 10 ) + 1 ]s
-sas hiv_synthesis.sas
+sas /home/cceapsc/sas/hiv-modelling/hiv_synthesis.sas
--- runsas.sh
+++ runsas.sh
@@ -5,12 +5,12 @@
 #$ -pe smp 16
 #S -N hivtestrun
 #$ -wd /home/cceapsc/Scratch
-#cd $TMPDIR
+cd $TMPDIR
 module load sas/9.4/64
-#cp $HOME/sas/hiv-modelling/model_runner.sas .
-#cp $HOME/sas/hiv-modelling/test_hiv_model.sas .
+cp $HOME/sas/hiv-modelling/model_runner.sas .
+cp $HOME/sas/hiv-modelling/hiv_synthesis_onefile.sas .
 #cp $HOME/sas/hiv-modelling/config.sas .
 
-sas $HOME/sas/hiv-modelling/model_runner.sas
+sas `pwd`/model_runner.sas
 
 
