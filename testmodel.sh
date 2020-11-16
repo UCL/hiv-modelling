3420937efa3cd137e3fb550b72db3525dc801bec
Peter Schmidt <peter.schmidt@ucl.ac.uk>
2020-11-16 14:27:08 +0000

    just try out running an array of jobs

--- testmodel.sh
+++ testmodel.sh
@@ -1,9 +1,15 @@
 #!/bin/bash -l
-#$ -l h_rt=08:00:00
-#$ -l mem=32G
-#$ -l tmpfs=72G
-#$ -N hivrunner
+#$ -S /bin/bash
+#$ -l h_rt=12:00:00
 #$ -pe smp 8
+#$ -t 1-100
+#$ -A HIVSynthMod
+#$ -l mem=36G
+#$ -j y
+#$ -N singlerunner
 #$ -wd /home/cceapsc/Scratch/output
+#$ -o /dev/null
+#$ -e /dev/null
+#$ -l tmpfs=100G
 module load sas/9.4/64
-sas $HOME/sas/hiv-modelling/model_runner.sas 
+sas $HOME/sas/hiv-modelling/single_runner.sas 
