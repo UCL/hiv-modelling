3188f95edba4213ef18449aa3bc332e28f9cd977
LoveleenB-M <63258736+LoveleenB-M@users.noreply.github.com>
2026-02-09 10:10:40 +0000

    update

---
+++ Zimbabwe/zim_package.sh
@@ -0,0 +1,46 @@
+#!/bin/bash -l
+inputdir="${HOME}"
+tmpfiles="out_"
+combinedsas="Genesis_Zim_a_07Jan26"
+runs="1000"
+jobname="hivmodel"
+model="hiv_synthesis_GenesisZim_07Jan26.sas"
+clock="h_rt=48:00:00"
+account="HIVSynthMod"
+
+while getopts a:i:o:r:j:m:c:t: flag
+do
+    case "${flag}" in
+        a) account=${OPTARG};;
+        i) inputdir=${OPTARG};;
+        o) combinedsas=${OPTARG};;
+        r) runs=${OPTARG};;
+        j) jobname=${OPTARG};;
+        m) model=${OPTARG};;
+        c) clock=${OPTARG};;
+        t) tmpfiles=${OPTARG};;
+    esac
+done
+finaloutdir="${HOME}/Scratch/${combinedsas}_${tmpfiles}"
+
+
+echo "===== JOB SUMMARY ======="
+echo "directory of input files: $inputdir";
+if [ -d $finaloutdir ]
+then
+    echo "Directory $finaloutdir exists"
+else
+    mkdir $finaloutdir
+    echo "output directory created: $finaloutdir"
+fi
+echo "final output SAS filename: $combinedsas";
+echo "tmp output file names start with: $tmpfiles"
+echo "runs: $runs";
+echo "jobname for model runs: $jobname";
+echo "clock is set to: $clock";
+echo "using SAS HIV model file: $model";
+echo "job is run on account: $account";
+qsub -N $jobname -t 1-$runs -tc 10 -wd $finaloutdir -P AllUsers -l $clock -v SASINPUT=$inputdir,SASOUTPUTDIR=$finaloutdir,SASMODEL=$model,SASTMPFILES=$tmpfiles $inputdir/testmodel.sh
+qsub -hold_jid $jobname -N concatenate -v SASINPUT=$inputdir,SASOUTPUT=$combinedsas,SASOUTPUTDIR=$finaloutdir,SASTMPFILES=$tmpfiles $inputdir/concatenate.sh 
+
+
